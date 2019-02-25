ea33f3c390c7f774e95c575041c75b1c953392cd
ferraridk <42999986+ferraridk@users.noreply.github.com>
2019-02-25 12:50:08 +0100

    Gui with fxid and handleactionbtn

--- TicTacToeUltimate/src/tictactoeultimate/gui/controller/TicTacViewController.java
+++ TicTacToeUltimate/src/tictactoeultimate/gui/controller/TicTacViewController.java
@@ -7,7 +7,10 @@ package tictactoeultimate.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -17,6 +20,169 @@ import javafx.fxml.Initializable;
 public class TicTacViewController implements Initializable
 {
 
+    @FXML
+    private Button btnA1;
+    @FXML
+    private Button btnA2;
+    @FXML
+    private Button btnA3;
+    @FXML
+    private Button btnA4;
+    @FXML
+    private Button btnA5;
+    @FXML
+    private Button btnA6;
+    @FXML
+    private Button btnA7;
+    @FXML
+    private Button btnA8;
+    @FXML
+    private Button btnA9;
+    @FXML
+    private Button btnD1;
+    @FXML
+    private Button btnD2;
+    @FXML
+    private Button btnD3;
+    @FXML
+    private Button btnD4;
+    @FXML
+    private Button btnD5;
+    @FXML
+    private Button btnD6;
+    @FXML
+    private Button btnD7;
+    @FXML
+    private Button btnD8;
+    @FXML
+    private Button btnD9;
+    @FXML
+    private Button btnG1;
+    @FXML
+    private Button btnG2;
+    @FXML
+    private Button btnG3;
+    @FXML
+    private Button btnG4;
+    @FXML
+    private Button btnG5;
+    @FXML
+    private Button btnG6;
+    @FXML
+    private Button btnG7;
+    @FXML
+    private Button btnG8;
+    @FXML
+    private Button btnG9;
+    @FXML
+    private Button btnB1;
+    @FXML
+    private Button btnB2;
+    @FXML
+    private Button btnB3;
+    @FXML
+    private Button btnB4;
+    @FXML
+    private Button btnB5;
+    @FXML
+    private Button btnB6;
+    @FXML
+    private Button btnB7;
+    @FXML
+    private Button btnB8;
+    @FXML
+    private Button btnB9;
+    @FXML
+    private Button btnE1;
+    @FXML
+    private Button btnE2;
+    @FXML
+    private Button btnE3;
+    @FXML
+    private Button btnE4;
+    @FXML
+    private Button btnE5;
+    @FXML
+    private Button btnE6;
+    @FXML
+    private Button btnE7;
+    @FXML
+    private Button btnE8;
+    @FXML
+    private Button btnE9;
+    @FXML
+    private Button btnH1;
+    @FXML
+    private Button btnH2;
+    @FXML
+    private Button btnH3;
+    @FXML
+    private Button btnH4;
+    @FXML
+    private Button btnH5;
+    @FXML
+    private Button btnH6;
+    @FXML
+    private Button btnH7;
+    @FXML
+    private Button btnH8;
+    @FXML
+    private Button btnH9;
+    @FXML
+    private Button btnC1;
+    @FXML
+    private Button btnC2;
+    @FXML
+    private Button btnC3;
+    @FXML
+    private Button btnC4;
+    @FXML
+    private Button btnC5;
+    @FXML
+    private Button btnC6;
+    @FXML
+    private Button btnC7;
+    @FXML
+    private Button btnC8;
+    @FXML
+    private Button btnC9;
+    @FXML
+    private Button btnF1;
+    @FXML
+    private Button btnF2;
+    @FXML
+    private Button btnF3;
+    @FXML
+    private Button btnF4;
+    @FXML
+    private Button btnF5;
+    @FXML
+    private Button btnF6;
+    @FXML
+    private Button btnF7;
+    @FXML
+    private Button btnF8;
+    @FXML
+    private Button btnF9;
+    @FXML
+    private Button btnI1;
+    @FXML
+    private Button btnI2;
+    @FXML
+    private Button btnI3;
+    @FXML
+    private Button btnI4;
+    @FXML
+    private Button btnI5;
+    @FXML
+    private Button btnI6;
+    @FXML
+    private Button btnI7;
+    @FXML
+    private Button btnI8;
+    @FXML
+    private Button btnI9;
+
     /**
      * Initializes the controller class.
      */
@@ -25,5 +191,9 @@ public class TicTacViewController implements Initializable
     {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
     
 }
