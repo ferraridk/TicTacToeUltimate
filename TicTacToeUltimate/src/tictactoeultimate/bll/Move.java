887b2c716f08b363c86f95fb827eb2e0d39299dd
ferraridk <42999986+ferraridk@users.noreply.github.com>
2019-02-26 10:44:09 +0100

    Move

--- TicTacToeUltimate/src/tictactoeultimate/bll/Move.java
+++ TicTacToeUltimate/src/tictactoeultimate/bll/Move.java
@@ -11,17 +11,35 @@ package tictactoeultimate.bll;
  */
 public class Move implements IMove
 {
+    int x=0;
+    int y=0;
 
-    @Override
-    public int getX()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Move(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public void setY(int y){
+        this.y=y;
+    }
+
+    public void setX(int x){
+        this.x=x;
     }
 
     @Override
-    public int getY()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getX() {
+        return x;
     }
-    
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "("+x+","+y+")";
+    }
+
 }
