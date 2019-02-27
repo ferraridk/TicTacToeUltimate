65e331de017be2f15a1a938fa9d977c400b5ada8
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-02-27 14:01:21 +0100

    Update Main and Added Jfoenix

--- TicTacToeUltimate/src/tictactoeultimate/TicTacToeUltimate.java
+++ TicTacToeUltimate/src/tictactoeultimate/TicTacToeUltimate.java
@@ -5,9 +5,12 @@
  */
 package tictactoeultimate;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,24 +23,9 @@ import javafx.stage.Stage;
 public class TicTacToeUltimate extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/tictactoeultimate/gui/view/Menu.fxml"));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
