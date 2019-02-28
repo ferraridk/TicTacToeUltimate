405ad5a3dc5644a6f8b7799c8fffcd45a7f6d6dc
simge2204 <44639861+simge2204@users.noreply.github.com>
2019-02-28 11:14:55 +0100

    tictacview

--- TicTacToeUltimate/src/tictactoeultimate/gui/controller/MenuController.java
+++ TicTacToeUltimate/src/tictactoeultimate/gui/controller/MenuController.java
@@ -5,9 +5,15 @@
  */
 package tictactoeultimate.gui.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,6 +22,9 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
+    @FXML
+    private AnchorPane menu;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +32,12 @@ public class MenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void BtnPressStart(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/tictactoeultimate/gui/view/TicTacView.fxml"));
+            Parent root = loader.load();
+            menu.getChildren().setAll(root);
+    }
     
 }
